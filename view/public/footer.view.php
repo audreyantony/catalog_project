6331c447cca0fad1b6f1cd40e8327ee5f12d20ec
audreyantony <web2020.audrey@gmail.com>
2020-10-10 13:19:41 +0200

    footer with connexion button (for admin)

--- view/public/footer.view.php
+++ view/public/footer.view.php
@@ -1,1 +1,9 @@
 <?php
+?>
+        <footer>
+            <div>
+                <p>Audrey Antony © 2020 || Catalog project ||<a href="?page=connect"> Connexion</a></p>
+            </div>
+        </footer>
+    </body>
+</html>
