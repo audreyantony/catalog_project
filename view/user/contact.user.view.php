84a6b3ab6e0173936c3bfd2c38ba248e85200a58
audreyantony <web2020.audrey@gmail.com>
2020-10-13 16:22:51 +0200

    Contact page layout

--- view/user/contact.user.view.php
+++ view/user/contact.user.view.php
@@ -1,1 +1,17 @@
 <?php
+?>
+
+<section id="contact">
+    <div class="img"></div>
+    <div class="inputs">
+        <h4></h4>
+    </div>
+</section>
+
+<section id="social-media">
+
+</section>
+
+<section id="map">
+
+</section>
