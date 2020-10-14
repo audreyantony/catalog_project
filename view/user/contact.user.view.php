806c455e58d8e277fad07bad2df0f32f50b3d7ce
audreyantony <web2020.audrey@gmail.com>
2020-10-14 10:14:03 +0200

    Contact page start layout

--- view/user/contact.user.view.php
+++ view/user/contact.user.view.php
@@ -1,1 +1,23 @@
 <?php
+?>
+
+<section id="contact">
+    <div class="img"></div>
+    <div class="inputs">
+        <h3>Contact</h3>
+        <form action="" method="post">
+            <input name="name" type="text" placeholder="Your name" required>
+            <input name="message" type="email" placeholder="Your email address" required>
+            <textarea name="message" id="" cols="30" rows="10" placeholder="Your comment or message" required></textarea>
+            <input type="submit">
+        </form>
+    </div>
+</section>
+
+<section id="social-media">
+
+</section>
+
+<section id="map">
+
+</section>
