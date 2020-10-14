c20863bd9d560274eabc78ae0782843de487aa5d
audreyantony <web2020.audrey@gmail.com>
2020-10-14 16:31:55 +0200

    map start and finish contact input layout

--- view/user/contact.user.view.php
+++ view/user/contact.user.view.php
@@ -4,31 +4,31 @@
 <section id="contact">
     <div class="img"></div>
     <div class="inputs">
-<<<<<<< HEAD
         <h3>Contact</h3>
         <form action="" method="post">
-            <input name="name" type="text" placeholder="Your name" required>
-            <input name="message" type="email" placeholder="Your email address" required>
-            <textarea name="message" id="" cols="30" rows="10" placeholder="Your comment or message" required></textarea>
-            <input type="submit">
+            <input class="form1" name="name" type="text" placeholder="Your name" required>
+            <input class="form2" name="message" type="email" placeholder="Your email address" required>
+            <textarea class="form3" name="message" placeholder="Your comment or message" required></textarea>
+            <input class="form4" type="submit" value="Send ►">
         </form>
-=======
         <h4></h4>
->>>>>>> 84a6b3ab6e0173936c3bfd2c38ba248e85200a58
     </div>
 </section>
 
 <section id="social-media">
-<<<<<<< HEAD
     <a href="https://fr-fr.facebook.com/" target="_blank"><img src="img/social-media/facebook.png" alt="facebook"></a>
     <a href="https://www.instagram.com/" target="_blank"><img src="img/social-media/instagram.png" alt="instagram"></a>
     <a href="https://www.pinterest.fr/" target="_blank"><img src="img/social-media/pinterest.png" alt="pinterest"></a>
     <a href="https://www.youtube.com/" target="_blank"><img src="img/social-media/youtube.png" alt="youtube"></a>
-=======
-
->>>>>>> 84a6b3ab6e0173936c3bfd2c38ba248e85200a58
 </section>
 
 <section id="map">
-
+    <div class="shop-map"></div>
+    <div class="list-shop">
+        <div id="leaflet-map">
+        </div>
+    </div>
 </section>
+
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+<script type="text/javascript" src="js/leaflet.js"></script>
