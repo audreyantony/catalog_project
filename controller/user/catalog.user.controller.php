8e88b118c3399d0124b1ea5b58ba2e79f5028628
audreyantony <web2020.audrey@gmail.com>
2020-10-20 12:25:38 +0200

    Test pagination function

--- controller/user/catalog.user.controller.php
+++ controller/user/catalog.user.controller.php
@@ -14,8 +14,7 @@ if (isset($_GET['catalog'])){
 
 $productNumber = mysqli_num_rows($product);
 $productByPage = 6;
-$currentPage = 1;
-$page = ceil($productNumber / $productByPage);
+
 
 $query = "SELECT *, LEFT(description_product, 60) AS descr FROM product LIMIT ".$productByPage." OFFSET ".(($currentPage-1)*$productByPage).";";
 $result = mysqli_query($db, $query);
@@ -24,9 +23,7 @@ while ($truc = mysqli_fetch_assoc($result)){
     echo $truc['name_product']."<br>";
 }
 
-for ($i =1; $i <$page; $i++){
-    echo "<a href=\"?page=catalog&catalog=".$i."\">| ".$i." |</a>";
-}
+echo pagination($productNumber, $currentPage,6);
 
 // CALLING VIEW
 include dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR . 'user' . DIRECTORY_SEPARATOR . 'catalog.user.view.php';
--- model/catalog.user.model.php
+++ model/catalog.user.model.php
@@ -15,3 +15,38 @@ function selectCategoryProducts($id, $db){
     $query = "SELECT id_product, name_category FROM product JOIN product_has_category ON id_product = product_id_product JOIN category ON category_id_category = id_category WHERE id_product = ".$id.";";
     return mysqli_query($db, $query);
 }
+
+function pagination($productNumber, $currentPage, $productByPage = 6){
+
+    $page = ceil($productNumber / $productByPage);
+    $pagination = "";
+
+    if($page == 0){
+        return "<p>There is no result</p>";
+    } else if ($page == 1){
+        return $pagination;
+    }
+
+    for ($i =1; $i <=$page; $i++){
+        if ($i==1){
+            if($i==$currentPage){
+                $pagination .= "<< ";
+            } else {
+                $pagination .= "<a href=\"?page=catalog&catalog=".($currentPage-1)."\"><< </a>";
+            }
+        }
+        if ($i=$currentPage){
+            $pagination .= $i;
+        } else {
+            $pagination .= "<a href=\"?page=catalog&catalog=".$i."\">| ".$i." |</a>";
+        }
+        if ($i=$page){
+            if ($i==$currentPage){
+                $pagination .= " >>";
+            } else {
+                $pagination .= "<a href=\"?page=catalog&catalog=".($currentPage+1)."\"> >></a>";
+            }
+        }
+    }
+    return $pagination;
+}
