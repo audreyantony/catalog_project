e809b59265dc726b9a08e2ee28a24f316a6b770f
audreyantony <web2020.audrey@gmail.com>
2020-10-29 15:45:03 +0100

    pagination finish

--- view/user/catalog.user.view.php
+++ view/user/catalog.user.view.php
@@ -1,52 +1,64 @@
-<section id="catalog">
-    <div class="products">
-        <?php
-        while ($item = mysqli_fetch_assoc($product)){
-            if ($item['instock_product'] == 1){
-                $picture = selectImgProducts($item['id_product'], $db);
-                $category = selectCategoryProducts($item['id_product'], $db);
-                if ($item['promoted_product'] == 1){
-                    echo "<div class='promoted_item'>";
-                    echo "<h2><img src=\"img/icon/thumb.png\" alt=\"thumb-up\"> ".$item['name_product']."</h2>";
-                    echo '<img class="img" src="img/'.$picture['name_img'].'" alt="'.$picture['alt_img'].'">';
-                    echo "<p class='descr'>".$item['descr']." ... </p>";
-                    if ($item['discount_product'] > 0 ){
-                        echo '<H5>DISCOUNT</H5>';
-                    } else {
-                        echo "<h5>".$item['price_product']." € </h5>";
+<section id="containercatalog">
+    <section id="find">
+        <h3>Search</h3>
+        <form action="" method="post">
+            <h4>Category :</h4>
+            <h4>Price range :</h4>
+        </form>
+
+    </section>
+    <section id="catalog">
+        <div class="products">
+            <?php
+            while ($item = mysqli_fetch_assoc($product)) {
+                if ($item['instock_product'] == 1) {
+                    $picture = selectImgProducts($item['id_product'], $db);
+                    $category = selectCategoryProducts($item['id_product'], $db);
+                    if ($item['promoted_product'] == 1) {
+                        echo "<div class='promoted_item'>";
+                        echo "<h2><img src=\"img/icon/thumb.png\" alt=\"thumb-up\"> " . $item['name_product'] . "</h2>";
+                        echo '<img class="img" src="img/' . $picture['name_img'] . '" alt="' . $picture['alt_img'] . '">';
+                        echo "<p class='descr'>" . $item['descr'] . " ... </p>";
+                        if ($item['discount_product'] > 0) {
+                            echo '<H5>DISCOUNT</H5>';
+                        } else {
+                            echo "<h5>" . $item['price_product'] . " € </h5>";
+                        }
+                        echo "<a href='?product=" . $item['id_product'] . "'><button>more on this ►</button></a>";
+                        echo "<p class='category'>";
+                        while ($cat = mysqli_fetch_assoc($category)) {
+                            echo $cat['name_category'] . " ";
+                        }
+                        echo "</p></div>";
+                    } else if ($item['promoted_product'] == 0) {
+                        echo "<div class='item'>";
+                        echo "<h2>" . $item['name_product'] . "</h2>";
+                        echo "<img class=\"img\" src=\"img/" . $picture['name_img'] . "\" alt=\"" . $picture['alt_img'] . "\">";
+                        echo "<p class='descr'>" . $item['descr'] . " ... </p>";
+                        if ($item['discount_product'] > 0) {
+                            echo '<H5>DISCOUNT</H5>';
+                        } else {
+                            echo "<h5>" . $item['price_product'] . " € </h5>";
+                        }
+                        echo "<a href='?product=" . $item['id_product'] . "'><button>more on this ►</button></a>";
+                        echo "<p class='category'>";
+                        while ($cat = mysqli_fetch_assoc($category)) {
+                            echo $cat['name_category'] . " ";
+                        }
+                        echo "</p></div>";
                     }
-                    echo "<a href='?product=".$item['id_product']."'><button>more on this ►</button></a>";
-                    echo "<p class='category'>";
-                    while ($cat = mysqli_fetch_assoc($category)){
-                        echo $cat['name_category']." ";
-                    }
-                    echo "</p></div>";
-                } else if ($item['promoted_product'] == 0) {
-                    echo "<div class='item'>";
-                    echo "<h2>".$item['name_product']."</h2>";
-                    echo "<img class=\"img\" src=\"img/".$picture['name_img']."\" alt=\"".$picture['alt_img']."\">";
-                    echo "<p class='descr'>".$item['descr']." ... </p>";
-                    if ($item['discount_product'] > 0 ){
-                        echo '<H5>DISCOUNT</H5>';
-                    } else {
-                        echo "<h5>".$item['price_product']." € </h5>";
-                    }
-                    echo "<a href='?product=".$item['id_product']."'><button>more on this ►</button></a>";
-                    echo "<p class='category'>";
-                    while ($cat = mysqli_fetch_assoc($category)){
-                        echo $cat['name_category']." ";
-                    }
-                    echo "</p></div>";
                 }
             }
-        }
-        ?>
-    </div>
-</section>
+            ?>
+        </div>
+        <div id="pagination">
+            <?php
+            echo pagination($productNumber, $currentPage, 6);
+            ?>
+        </div>
 
-<div>
-    <a href="?page=catalog&catalog="></a>
-</div>
+    </section>
+</section>
 
 <script type="text/javascript">
     function dicountedPrice(price, discount) {
