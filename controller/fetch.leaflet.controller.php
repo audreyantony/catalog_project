e21fc89e1f49ede904b9647da1709ed8a098e5a9
audreyantony <web2020.audrey@gmail.com>
2020-11-09 15:42:04 +0100

    fetch for leaflet

---
+++ controller/fetch.leaflet.controller.php
@@ -0,0 +1,20 @@
+<?php
+
+// CALLING MODEL
+include dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . 'config.php';
+include dirname(__DIR__) . DIRECTORY_SEPARATOR . 'model' . DIRECTORY_SEPARATOR . 'DBconnect.model.php';
+include dirname(__DIR__) . DIRECTORY_SEPARATOR . 'model' . DIRECTORY_SEPARATOR . 'contact.user.model.php';
+
+$db =DBconnect();
+
+$shops = shopsLatLongSelect($db);
+
+$data = [];
+
+while ($shop = mysqli_fetch_assoc($shops)){
+    $data[] = $shop;
+
+}
+
+header('Content-Type: application/json');
+echo json_encode($data);
--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -6,11 +6,22 @@ L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.pn
 }).addTo(map);
 
 
-let shop3 = L.marker([48.815, 2.319]).addTo(map);
+/*let shop3 = L.marker([48.815, 2.319]).addTo(map);
 shop3.bindPopup("<p> ANA Montrouge <br> 01.98.76.54.32</p>").openPopup();
 
 let shop1 = L.marker([48.84, 2.33]).addTo(map);
 shop1.bindPopup("<p> ANA Raspail <br> 01.23.45.67.89</p>").openPopup();
 
 let shop2 = L.marker([48.87, 2.31]).addTo(map);
-shop2.bindPopup("<p> ANA Champs Élysées <br> 01.29.38.47.56</p>").openPopup();
+shop2.bindPopup("<p> ANA Champs Élysées <br> 01.29.38.47.56</p>").openPopup();*/
+
+fetch('../controller/fetch.leaflet.controller.php').then(function (response){
+    return response.text();
+}).then(function(data) {
+    let shops = JSON.parse(data);
+
+    for (let i = 0 ; i < shops.length ; i++){
+        let shop = L.marker([shops[i].lat_shop, shops[i].long_shop]).addTo(map);
+        shop.bindPopup("<p> " + shops[i].name_shop + " <br> " + shops[i].description_shop + " </p>").openPopup();
+    }
+});
