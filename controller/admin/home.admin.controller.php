159270bd6685816e7a48e42ba0cd8f659e0eafec
audreyantony <web2020.audrey@gmail.com>
2020-11-23 15:12:10 +0100

    comment -end

--- controller/admin/home.admin.controller.php
+++ controller/admin/home.admin.controller.php
@@ -1,5 +1,6 @@
 <?php
 
+// REDIRECT IF NOT CONNECTED
 if(!isset($_SESSION['id_session'])|| $_SESSION['id_session']!==session_id()){
     header('Location: ?admin=connect');
     exit();
@@ -9,14 +10,18 @@ if(!isset($_SESSION['id_session'])|| $_SESSION['id_session']!==session_id()){
 include dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'model' . DIRECTORY_SEPARATOR . 'admin.model.php';
 
 // CONTROLLER CODE
+
+// SELECT THE SHOPS
 $shops = selectAllShops($db);
 
+// SELECT THE CATEGORIES
 $categories = selectAllCategories($db);
 
+// SELECT THE IMAGES
 $images = selectAllImages($db);
 
+// SELECT THE PRODUCT
 $products = selectAllProductsSmall($db);
-
 
 // CALLING VIEW
 include dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR . 'admin' . DIRECTORY_SEPARATOR . 'home.admin.view.php';
