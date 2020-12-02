c5cbd665874cad4144b538bb9f5e32ff06eb9c49
audreyantony <web2020.audrey@gmail.com>
2020-12-02 17:01:42 +0100

    touch ups

--- view/admin/delete.image.admin.view.php
+++ view/admin/delete.image.admin.view.php
@@ -6,7 +6,7 @@
     <div class="card text-right" style="width: 36rem;">
         <div class="card-body">
             <h5 class="card-title text-left"><?=$img['name_img']?></h5>
-            <div class ="text-left"><img class="bg-dark border" width="230px" src="img/<?=$img['name_img']?>" alt="<?=$img['alt_img']?>"></div>
+            <div class ="text-left"><img class="bg-dark border" width="230px" src="img/upload/<?=$img['name_img']?>" alt="<?=$img['alt_img']?>"></div>
             <h6 class="card-subtitle mb-2 mt-2 text-muted text-left">This image ID = <?=$img['id_img']?></h6>
             <p class="card-text text-left">Alt : <?=$img['id_img']?></p>
             <a href="?admin=crud&updateimage=<?=$img['id_img']?>"><button type="button" class="btn btn-primary ml-3 mb-2">UPDATE</button></a>
